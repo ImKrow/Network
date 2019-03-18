bc2dc6013cccae25e4e831f2e95cf3ba2e1e9c36
James B. Kevlin <kevlinbjames@gmail.com>
2019-03-18 11:47:08 -0500

    Setting up tiles

--- src/MapTile.java
+++ src/MapTile.java
@@ -12,11 +12,17 @@ public class MapTile extends Rectangle {
     public MapTile(double x, double y){
         this.x = x;
         this.y = y;
+        draw();
 
     }
     public void draw(){
-//        this.setLayoutX(x*width);
-//        this.setLayoutY(y*height);
-        this.setFill(Color.GREEN);
+        System.out.println("mk");
+        this.setWidth(width);
+        this.setHeight(height);
+        if(x %2 != 0 && y % 2 ==0) {
+            this.setFill(Color.GREEN);
+        } else{
+            this.setFill(Color.BLUE);
+        }
     }
 }
